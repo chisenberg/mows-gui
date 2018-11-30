d0e74bfa886d659e7febf6e18386f410cd4bd7c4
chisenberg <christianisenberg@gmail.com>
2018-11-29 22:37:35 -0200

    Getting temp and humid data

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,8 @@
       <!-- Left boxes -->
       <div id="box_container">
         <box title="Chuva mm" color="#18afd5" value="0"/>
-        <box title="Temp °C" color="#1bf162" value="0" min="--" max="--"/>
-        <box title="Umidade %" color="#fb8b27" value="0" min="--" max="--"/>
+        <box title="Temp °C" color="#1bf162" :value="temp" :min="tempMin" :max="tempMax"/>
+        <box title="Umidade %" color="#fb8b27" :value="humid" :min="humidMin" :max="humidMax"/>
         <box title="Press mmHg" color="#f13c38" value="0" min="--" max="--"/>
         <box title="Vento Km/h" color="#ccc" value="0" min="0" max="--"/>
         <box title="Previsão" color="#18afd5" value="?"/>
@@ -24,14 +24,41 @@
 </template>
 
 <script>
+const axios = require('axios');
 import TopBar from "./components/TopBar.vue";
 import Box from "./components/Box.vue";
 
 export default {
-  name: "app",
   components: {
     TopBar,
     Box
+  },
+
+  data: function() {
+    return {
+      temp: null,
+      tempMin: null,
+      tempMax: null,
+      humid: null,
+      humidMin: null,
+      humidMax: null,
+    }
+  },
+
+  mounted() {
+
+    var self = this;
+    axios.get('http://192.168.25.2/api/now')
+    .then(function (response) {
+      let data = response.data[0];
+      self.temp = data.temp;
+      self.tempMin = data.tempMin;
+      self.tempMax = data.tempMax;
+      self.humid = data.humid;
+      self.humidMin = data.humidMin;
+      self.humidMax = data.humidMax;
+    });
+    
   }
 };
 </script>
